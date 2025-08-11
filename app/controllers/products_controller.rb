f6089262f58d5df84c39006ea40187f8bb47f239
AlexF6 <perol2894@gmail.com>
2025-08-11 16:14:15 -0500

    feat: improve view show, edit, index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.expect(product: [ :name, :description, :price, :stock ])
+      params.expect(product: [ :name, :description, :price, :stock, :image_src ])
     end
 end
