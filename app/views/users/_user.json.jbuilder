5002af3f3683c93fbef9821ff3ae2e05b5b33205
AlexF6 <perol2894@gmail.com>
2025-08-12 16:26:42 -0500

    feat: add auth

--- app/views/users/_user.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! user, :id, :username, :email, :date_of_birth, :admin, :created_at, :updated_at
-json.url user_url(user, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   get "pages/home"
   resources :products
   resources :users
